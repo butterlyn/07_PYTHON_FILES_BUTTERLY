8215ee5eab399749139801ab08ce1e149232766c
butterlyn <21714914@student.uwa.edu.au>
2020-08-26 01:20:06 +0800

    Copmleted LinkedIn course 'Learning Python' Ch2 'Loops' and added docstring to 'conditionals'

--- Ex_Files_Learning_Python/Ch2/conditionals_start.py
+++ Ex_Files_Learning_Python/Ch2/conditionals_start.py
@@ -35,25 +35,47 @@ def conditionalFlow(x, y):
   #------------------------ Output ----------------------------
   return result
 
-def conditionalStatement(x,y):
+def conditionalStatement(x=0,y=0):
   """
-  Evaluates two numbers and returns string saying which variable is greater
+  conditionalStatement(x,y)
 
-  EXAMPLE:
-  print(conditionalFlow(3,4))
+  Evaluates two numbers and returns string saying which variable is greater.
 
-  PARAMETERS:
-    x,y:  Numbers to be evaluated
+  Parameters
+  ----------
+  x, y : int, optional
+    Numbers to be evaluated (the default is 0).
+  z : {'first option','4','3','P'}
+    Parameter with only a set number of possible values.
 
-  RETURNS:
-    st:   Statement saying which variable is larger or smaller
+  Returns
+  -------
+  st : str
+    Statement saying which variable is larger or smaller.
   
-  25/08/20: Created
+  Raises
+  ------
+  CustomError1
+    A custom error.
+  
+  See Also
+  --------
+  conditionalFlow : Same as conditionalStatement except using flow statements.
+  func1, func3
 
+  Notes
+  -----  
+  25/08/20: Created
   Nicholas Butterly
+
+  Examples
+  --------
+  >>> print(conditionalFlow(3,4))
+  Second number is larger
+
   """
   result = 0 # set default funciton output
-  
+
   #----------------- Input Error Handling ---------------------
   #----------------------- Function ---------------------------
   st = "Second number is larger" if(x<y) else "Numbers are equal?"
@@ -64,10 +86,12 @@ def conditionalStatement(x,y):
   return result
 
 def main():
-  print(conditionalFlow(44,4))
+  print(conditionalFlow(3,4))
   print(conditionalStatement(0,4))
 
 if __name__ == "__main__":
   main()
 
 # THERE IS NO 'SWITCH' FLOW CONTROL IN PYTHON, ONLY ELIF STATEMENTS
+# For docstring formatting ==> https://numpydoc.readthedocs.io/en/latest/format.html
+# For python code formatting ===> https://www.python.org/dev/peps/pep-0008/#documentation-strings
--- Ex_Files_Learning_Python/Ch2/loops_start.py
+++ Ex_Files_Learning_Python/Ch2/loops_start.py
@@ -4,21 +4,24 @@
 
 def main():
   x = 0
-
-  # define a while loop
-
-
-  # define a for loop
-
-
-  # use a for loop over a collection
-    
- 
-  # use the break and continue statements
-
-
-  #using the enumerate() function to get index 
-
+  while(x<5):
+    print(x)
+    x = x+1
+  print()  
+  for i in range(4):
+    print(i)
+  print()
+  for i in [1,4,"hi"]:
+    print(i)
+  print()
+  for i,d in enumerate([1,4,"hi"],11):
+    print(i,d)
+  print()
+  for i in range(1,14,2):
+    print(i)
 
 if __name__ == "__main__":
   main()
+
+
+# x = x++ is NOT VALID
