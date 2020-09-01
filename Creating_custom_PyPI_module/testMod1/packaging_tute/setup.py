a8851bae3af85dfb17e6c95a637a77c2e3bc8022
butterlyn <21714914@student.uwa.edu.au>
2020-09-02 06:35:49 +0800

    modified Creating_custom_PyPI_module setup.py file to include .md to .txt conversion. custom PyPI module project superseded by PYTHON_MODULE_TEMPLATE github repo

--- Creating_custom_PyPI_module/testMod1/packaging_tute/setup.py
+++ Creating_custom_PyPI_module/testMod1/packaging_tute/setup.py
@@ -1,17 +1,42 @@
+## DO NOT CHANGE
 import setuptools
+from markdown import Markdown
+from io import StringIO
+
+def unmark_element(element, stream=None):
+    if stream is None:
+        stream = StringIO()
+    if element.text:
+        stream.write(element.text)
+    for sub in element:
+        unmark_element(sub, stream)
+    if element.tail:
+        stream.write(element.tail)
+    return stream.getvalue()
+
+Markdown.output_formats["plain"] = unmark_element
+__md = Markdown(output_format="plain")
+__md.stripTopLevelTags = False
+
+def unmark(text):
+    return __md.convert(text)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+    long_description = unmark(long_description)
+    print(long_description)
 
+
+## CUSTOMISE WITH PACKAGE DETAILS. `#` entries require updating
 setuptools.setup(
-    name="example-pkg-butterlyn", # Replace with your own username
-    version="0.0.2",
+    name="example-pkg-butterlyn",
+    version="0.0.2", # update to latest release version
     author="Nicholas Butterly",
     author_email="butterlyn888@gmail.com",
-    description="A small example package",
+    description="A small example package", # update with one-sentence package description
     long_description=long_description,
-    long_description_content_type="text/markdown", # specifies .txt or .md README file
-    url="https://github.com/pypa/sampleproject",
+    long_description_content_type="text/markdown",
+    url="https://github.com/butterlyn/PACKAGE_NAME_IN_GIT", # specify url to package in github
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
